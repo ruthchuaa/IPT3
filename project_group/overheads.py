f98ce83ce734731c7459738d7a4f445beed6148f
ss102ji <s10243171@connect.np.edu.sg>
2022-07-31 22:45:19 +0800

    overheads

--- project_group/overheads.py
+++ project_group/overheads.py
@@ -1,2 +1,24 @@
-def sub(a,b):
-    return a - b
+from pathlib import Path
+import csv
+file_path = Path.cwd()/"project_group"/"csv_reports"/"Overheads.csv"
+
+overheads = []
+
+if file_path.exists():
+   with file_path.open(mode = "r",encoding = 'ascii', errors = 'ignore', newline='') as open:
+    all = csv.reader(open)
+    next(all)
+    
+    for items in all:
+        overheads.append({items[0] : float(items[1])})
+
+# print(overheads)
+allov = []
+
+for lists in overheads:
+    for category, ov in lists.items():
+        allov.append(ov)
+        highest = [max(allov)]
+        if ov in highest:
+            print(category) 
+    
