99de8a8d674f377711d516420befaefbe1bc396f
ss102ji <s10243171@connect.np.edu.sg>
2022-08-02 07:17:15 +0800

    use list to read files

--- project_group/read_files.py
+++ project_group/read_files.py
@@ -1,8 +1,6 @@
 from pathlib import Path
 import csv
 
-
-cashonhand = []
 def csvread(csvfile, list):
     
     allfilepath = Path.cwd()/"project_group"/"csv_reports"
@@ -12,13 +10,3 @@ def csvread(csvfile, list):
         next(reader) 
         for line in reader:
             list.append(line)
-
-        
-
-
-csvread('Cash on Hand.csv', cashonhand)
-print(cashonhand)
-
-
-
-
