8dee97daa5daba64650cf8a86872a8bb6519e149
yiyingleck <yiying_04@hotmail.com>
2022-08-02 14:56:32 +0800

    update

--- project_group/cash_on_hand.py
+++ project_group/cash_on_hand.py
@@ -1,35 +1,38 @@
 from pathlib import Path
 from read_files import csvread
 
-netCOH = []
-allCOH = []
-alldata = []
+def cash_on_hand():
+    netCOH = []
+    allCOH = []
+    alldata = []
 
-readfiles = csvread('Cash on Hand.csv', alldata)
+    readfiles = csvread('Cash on Hand.csv', alldata)
 
-for lines in alldata:
-    netCOH.append(int(lines[1]))
-    allCOH.append({lines[0]: int(lines[1])})
-    
-print(netCOH)
-print(allCOH)
-deficit = []
-
-for index, value in enumerate(netCOH):
-    if value > netCOH[0]:
-        diff = netCOH[index] - netCOH[index-1]
-        if diff < 0 :
-            deficit.append([value, diff])
-
-
-for items in allCOH:
-    for days, number in items.items():
-        for one, two in deficit:
-            if number == one:
-                print(two)
-
+    for lines in alldata:
+        netCOH.append(int(lines[1]))
+        allCOH.append({lines[0]: int(lines[1])})
         
+    print(netCOH)
+    print(allCOH)
+    deficit = []
+
+    for index, value in enumerate(netCOH):
+        if value > netCOH[0]:
+            diff = netCOH[index] - netCOH[index-1]
+            if diff < 0 :
+                deficit.append([value, diff])
+
+
+    for items in allCOH:
+        for days, number in items.items():
+            for one, two in deficit:
+                if number == one:
+                    print(two)
+
+cash_on_hand()
+
+            
 
 
 
-        
+            
