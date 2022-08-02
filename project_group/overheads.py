472064dbfa444c5c060e581ddeab2fdc15481200
ss102ji <s10243171@connect.np.edu.sg>
2022-08-02 14:48:01 +0800

    ov

--- project_group/overheads.py
+++ project_group/overheads.py
@@ -2,23 +2,24 @@ from pathlib import Path
 import csv
 file_path = Path.cwd()/"project_group"/"csv_reports"/"Overheads.csv"
 
-overheads = []
+def ov() :
+    overheads = []
 
-if file_path.exists():
-   with file_path.open(mode = "r",encoding = 'ascii', errors = 'ignore', newline='') as open:
-    all = csv.reader(open)
-    next(all)
-    
-    for items in all:
-        overheads.append({items[0] : float(items[1])})
+    if file_path.exists():
+        with file_path.open(mode = "r",encoding = 'ascii', errors = 'ignore', newline='') as open:
+            all = csv.reader(open)
+            next(all)
+            for items in all:
+                overheads.append({items[0] : float(items[1])})
 
-# print(overheads)
-allov = []
+    # print(overheads)
+    allov = []
 
-for lists in overheads:
-    for category, ov in lists.items():
-        allov.append(ov)
-        highest = [max(allov)]
-        if ov in highest:
-            print(category, ov)
-            
+    for lists in overheads:
+        for category, ov in lists.items():
+            allov.append(ov)
+            highest = [max(allov)]
+            if ov in highest:
+                print(category, ov)
+
+ov()
