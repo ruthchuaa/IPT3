21e34e44ddf14e35e1a49551135630591edd76f7
ss102ji <s10243171@connect.np.edu.sg>
2022-08-02 16:27:56 +0800

    update

--- project_group/cash_on_hand.py
+++ project_group/cash_on_hand.py
@@ -1,40 +1,32 @@
 from pathlib import Path
 from read_files import csvread
 
-# def cash_on_hand():
-netCOH = []
-allCOH = []
-alldata = []
+def cash_on_hand():
+    netCOH = []
+    allCOH = []
+    alldata = []
 
-readfiles = csvread('Cash on Hand.csv', alldata)
+    readfiles = csvread('Cash on Hand.csv', alldata)
 
-for lines in alldata:
-    netCOH.append(int(lines[1]))
-    allCOH.append({lines[0]: int(lines[1])})
-    
-print(netCOH)
-print(allCOH)
-deficit = []
+    for lines in alldata:
+        netCOH.append(int(lines[1]))
+        allCOH.append({lines[0]: int(lines[1])})
+        
+    deficit = []
 
-for index, value in enumerate(netCOH):
-    if value > netCOH[0]:
-        diff = netCOH[index] - netCOH[index-1]
-        if diff < 0 :
-            deficit.append([value, diff])
+    for index, value in enumerate(netCOH):
+        if value > netCOH[0]:
+            diff = netCOH[index] - netCOH[index-1]
+            if diff < 0 :
+                deficit.append([value, diff])
 
 
-for items in allCOH:
-    for days, number in items.items():
-        for one, two in deficit:
-            if number == one:
-                print(two)
-else:
-    print("no")
+    for items in allCOH:
+        for days, number in items.items():
+            for one, two in deficit:
+                if number == one:
+                    print(two)
+    else:
+        print(f"[CASH SURPLUS] CASH ON HAND EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
 
-# cash_on_hand()
-
-            
-
-
-
-            
+cash_on_hand()
