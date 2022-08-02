8c71d9ee297739bf66e28715282605a7e61848cb
yiyingleck <yiying_04@hotmail.com>
2022-08-02 14:50:00 +0800

    update

--- project_group/overheads.py
+++ project_group/overheads.py
@@ -1,24 +1,30 @@
 from pathlib import Path
 import csv
-file_path = Path.cwd()/"project_group"/"csv_reports"/"Overheads.csv"
 
-overheads = []
+def overheads():
+    file_path = Path.cwd()/"project_group"/"csv_reports"/"Overheads.csv"
 
-if file_path.exists():
-   with file_path.open(mode = "r",encoding = 'ascii', errors = 'ignore', newline='') as open:
-    all = csv.reader(open)
-    next(all)
-    
-    for items in all:
-        overheads.append({items[0] : float(items[1])})
+    overheads = []
 
-# print(overheads)
-allov = []
+    if file_path.exists():
+        with file_path.open(mode = "r",encoding = 'ascii', errors = 'ignore', newline='') as open:
+            all = csv.reader(open)
+            next(all)
+        
+            for items in all:
+                overheads.append({items[0] : float(items[1])})
 
-for lists in overheads:
-    for category, ov in lists.items():
-        allov.append(ov)
-        highest = [max(allov)]
-        if ov in highest:
-            print(category, ov)
-            
+    # print(overheads)
+    allov = []
+
+
+    for lists in overheads:
+        for category, ov in lists.items():
+            allov.append(ov)
+            highest = [max(allov)]
+            if ov in highest:
+                print(f"[HIGHEST OVERHEADS] {category.upper(), ov}")
+
+overheads()
+
+                
--- project_group/writing.py
+++ project_group/writing.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import api
+import overheads
 import profit_loss
 
 file_path= Path.cwd()/"project_group"/"csv_reports"/"summary_report.txt"
@@ -9,6 +10,7 @@ print(file_path.exists())
 # list= [f"[REAL TIME CURRENCY CONVERSION RATE] USD1 = SGD{api.all}"]
 with file_path.open(mode = "w") as file:
     file.writelines(f"[REAL TIME CURRENCY CONVERSION RATE] USD1 = SGD{api.all}")
+    file.writelines(f"\n{overheads.lists}")
 
 
 
