1fb0fcc2407b53c8bbdf54cb13292d82adcb4655
yiyingleck <yiying_04@hotmail.com>
2022-08-02 11:50:43 +0800

    update

--- project_group/cashonhand.py
+++ project_group/cashonhand.py
@@ -1,40 +1,33 @@
 from pathlib import Path
-import csv
-file_path = Path.cwd()/"project_group"/"csv_reports"/"Cash on Hand.csv"
+from read_files import csvread
 
 netCOH = []
 allCOH = []
+alldata = []
 
-if file_path.exists():
-   with file_path.open(mode = "r",encoding = 'ascii', errors = 'ignore', newline='') as open:
-    cash = csv.reader(open)
-    next(cash)
+readfiles = csvread('Cash on Hand.csv', alldata)
+
+for lines in alldata:
+    netCOH.append(int(lines[1]))
+    allCOH.append({lines[0]: int(lines[1])})
     
-    for lines in cash:
-        netCOH.append(int(lines[1]))
-        allCOH.append({lines[0]: int(lines[1])})
-    
+print(netCOH)
+print(allCOH)
 deficit = []
 
 for index, value in enumerate(netCOH):
     if value > netCOH[0]:
         diff = netCOH[index] - netCOH[index-1]
         if diff < 0 :
-            deficit.append(value)
-
-
-
-print(allCOH)
-print(deficit)
+            deficit.append([value, diff])
 
 
 for items in allCOH:
-    for days, number in items.items() :
-        if number in deficit :
-             print(days)
-        
-        else:
-            print("No cash deficit found")
+    for days, number in items.items():
+        for one, two in deficit:
+            if number == one:
+                print(two)
+
         
 
 
