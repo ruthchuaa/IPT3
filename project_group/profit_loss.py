bed31b33eb8351d82215a260f2c2e1ec935d7629
ss102ji <s10243171@connect.np.edu.sg>
2022-08-02 14:44:38 +0800

    define

--- project_group/profit_loss.py
+++ project_group/profit_loss.py
@@ -1,28 +1,30 @@
 from pathlib import Path
 from read_files import csvread
 
-netprofit = []
-allprofits = []
-alldata = []
+def test():
+    netprofit = []
+    allprofits = []
+    alldata = []
 
-readfiles = csvread('Profits and loss.csv', alldata)
+    readfiles = csvread('Profits and loss.csv', alldata)
 
-for lines in alldata:
-    netprofit.append(int(lines[4]))
-    allprofits.append({lines[0]: int(lines[4])})
-    
-deficit = []
+    for lines in alldata:
+        netprofit.append(int(lines[4]))
+        allprofits.append({lines[0]: int(lines[4])})
+        
+    deficit = []
 
-for index, value in enumerate(netprofit):
-    if value > netprofit[0]:
-        diff = netprofit[index] - netprofit[index-1]
-        if diff < 0 :
-            deficit.append([value, diff])
+    for index, value in enumerate(netprofit):
+        if value > netprofit[0]:
+            diff = netprofit[index] - netprofit[index-1]
+            if diff < 0 :
+                deficit.append([value, diff])
 
 
-for items in allprofits:
-    for days, number in items.items():
-        for one, two in deficit:
-            if number == one:
-                print(two)
+    for items in allprofits:
+        for days, number in items.items():
+            for one, two in deficit:
+                if number == one:
+                    print(two)
 
+test()
