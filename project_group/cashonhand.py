d38de7d37f824937fd71e09341f798e16f807dd5
yiyingleck <yiying_04@hotmail.com>
2022-08-02 11:40:21 +0800

    update

---
+++ project_group/cashonhand.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+import csv
+file_path = Path.cwd()/"project_group"/"csv_reports"/"Cash on Hand.csv"
+
+netCOH = []
+allCOH = []
+
+if file_path.exists():
+   with file_path.open(mode = "r",encoding = 'ascii', errors = 'ignore', newline='') as open:
+    cash = csv.reader(open)
+    next(cash)
+    
+    for lines in cash:
+        netCOH.append(int(lines[1]))
+        allCOH.append({lines[0]: int(lines[1])})
+    
+deficit = []
+
+for index, value in enumerate(netCOH):
+    if value > netCOH[0]:
+        diff = netCOH[index] - netCOH[index-1]
+        if diff < 0 :
+            deficit.append(value)
+
+
+
+print(allCOH)
+print(deficit)
+
+
+for items in allCOH:
+    for days, number in items.items() :
+        if number in deficit :
+             print(days)
+        
+        else:
+            print("No cash deficit found")
+        
+
+
+
+        
