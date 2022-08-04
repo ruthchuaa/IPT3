edfb2cfd0db55c403c6952f06fb2cc17e3b7184c
ss102ji <s10243171@connect.np.edu.sg>
2022-08-04 11:56:00 +0800

    exception handle

--- project_group/overheads.py
+++ project_group/overheads.py
@@ -1,25 +1,25 @@
+from os import read
 from pathlib import Path
 import csv
 import api
+import read_files
 
 def overheads():
     file_path = Path.cwd()/"project_group"/"csv_reports"/"Overheads.csv"
 
-    overheads = []
+    overhead = []
+    alldata = []
 
-    if file_path.exists():
-        with file_path.open(mode = "r",encoding = 'ascii', errors = 'ignore', newline='') as open:
-            all = csv.reader(open)
-            next(all)
+    readfile = read_files.csvread('Overheads.csv', alldata)
         
-            for items in all:
-                overheads.append({items[0] : float(items[1])})
+    for items in alldata:
+        overhead.append({items[0] : float(items[1])})
 
     # print(overheads)
     allov = []
 
 
-    for lists in overheads:
+    for lists in overhead:
         for category, ov in lists.items():
             allov.append(ov)
             highest = [max(allov)]
--- project_group/read_files.py
+++ project_group/read_files.py
@@ -6,7 +6,7 @@ def csvread(csvfile, list):
     
     allfilepath = Path.cwd()/"project_group"/"csv_reports"
     newpath = allfilepath/csvfile
-    with newpath.open(mode = "r",encoding = "UTF-8", newline="") as file:
+    with newpath.open(mode = "r",encoding = 'ascii', errors = 'ignore', newline='') as file:
         reader = csv.reader(file)
         next(reader) 
         for line in reader:
