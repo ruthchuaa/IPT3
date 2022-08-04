20573a2ceed695e6344082bd0c8df327041a60f8
10242344A <s10242344@connect.np.edu.sg>
2022-08-04 22:21:31 +0800

    commit

--- project_group/overheads.py
+++ project_group/overheads.py
@@ -3,23 +3,23 @@ from pathlib import Path
 import csv
 import api
 import read_files
-
+#
 def overheads():
     try:
         file_path = Path.cwd()/"project_group"/"csv_reports"/"Overheads.csv"
-
+#
         overhead = []
         alldata = []
 
         readfile = read_files.csvread('Overheads.csv', alldata)
-            
+         #   
         for items in alldata:
             overhead.append({items[0] : float(items[1])})
 
         # print(overheads)
         allov = []
 
-
+#
         for lists in overhead:
             for category, ov in lists.items():
                 allov.append(ov)
