cc479b14c19cc45566254ef6eca7770b7f591551
ss102ji <s10243171@connect.np.edu.sg>
2022-08-04 22:19:46 +0800

    overheads error handling

--- project_group/overheads.py
+++ project_group/overheads.py
@@ -5,26 +5,29 @@ import api
 import read_files
 
 def overheads():
-    file_path = Path.cwd()/"project_group"/"csv_reports"/"Overheads.csv"
+    try:
+        file_path = Path.cwd()/"project_group"/"csv_reports"/"Overheads.csv"
 
-    overhead = []
-    alldata = []
+        overhead = []
+        alldata = []
 
-    readfile = read_files.csvread('Overheads.csv', alldata)
-        
-    for items in alldata:
-        overhead.append({items[0] : float(items[1])})
+        readfile = read_files.csvread('Overheads.csv', alldata)
+            
+        for items in alldata:
+            overhead.append({items[0] : float(items[1])})
 
-    # print(overheads)
-    allov = []
+        # print(overheads)
+        allov = []
 
 
-    for lists in overhead:
-        for category, ov in lists.items():
-            allov.append(ov)
-            highest = [max(allov)]
-            if ov in highest:
-                return (f"[HIGHEST OVERHEADS] {category.upper()}: SGD{api.convert(ov):.2f}")
+        for lists in overhead:
+            for category, ov in lists.items():
+                allov.append(ov)
+                highest = [max(allov)]
+                if ov in highest:
+                    return (f"[HIGHEST OVERHEADS] {category.upper()}: SGD{api.convert(ov):.2f}")
+    except Exception as e:
+        return('Error')
 
 
 
