221b759565603b6eea71e2a8dada7fd9a2de94c9
ss102ji <s10243171@connect.np.edu.sg>
2022-08-04 22:13:38 +0800

    cash on hand error

--- project_group/cash_on_hand.py
+++ project_group/cash_on_hand.py
@@ -3,35 +3,39 @@ from read_files import csvread
 import api
 #
 def cash_on_hand():
-    netCOH = []
-    allCOH = []
-    alldata = []
+    try:
 
-    readfiles = csvread('Cash on Hand.csv', alldata)
+        netCOH = []
+        allCOH = []
+        alldata = []
 
-    for lines in alldata:
-        netCOH.append(int(lines[1]))
-        allCOH.append({lines[0]: int(lines[1])})
-        
-    deficit = []
+        readfiles = csvread('Cash on Hand.csv', alldata)
 
-    for index, value in enumerate(netCOH):
-        if value > netCOH[0]:
-            diff = netCOH[index] - netCOH[index-1]
-            if diff < 0 :
-                deficit.append([value, diff])
-        
-    list =[]
+        for lines in alldata:
+            netCOH.append(int(lines[1]))
+            allCOH.append({lines[0]: int(lines[1])})
+            
+        deficit = []
 
-    for items in allCOH:
-        for days, number in items.items():
-            for one, two in deficit:
-                if number == one:
-                    amt = abs(two)
-                    re = (f'[CASH DEFICIT] DAY : {days}, AMOUNT : SGD{api.convert(amt):.2f}')
-                    list.append(re)
-                    
-    return(list)
+        for index, value in enumerate(netCOH):
+            if value > netCOH[0]:
+                diff = netCOH[index] - netCOH[index-1]
+                if diff < 0 :
+                    deficit.append([value, diff])
+            
+        list =[]
+
+        for items in allCOH:
+            for days, number in items.items():
+                for one, two in deficit:
+                    if number == one:
+                        amt = abs(two)
+                        re = (f'[CASH DEFICIT] DAY : {days}, AMOUNT : SGD{api.convert(amt):.2f}')
+                        list.append(re)
+                        
+        return(list)
+    except Exception as e:
+        return ('Error')
 
 
 
