5ceed27cc3ef3e4b3126b9f8d26a9892666cf3be
ruthchuaa <ruthchua05@gmail.com>
2022-08-04 18:43:46 +0800

    comment commit

--- project_group/cash_on_hand.py
+++ project_group/cash_on_hand.py
@@ -1,28 +1,28 @@
 from pathlib import Path
 from read_files import csvread
 import api
-
+#
 def cash_on_hand():
     netCOH = []
     allCOH = []
     alldata = []
 
     readfiles = csvread('Cash on Hand.csv', alldata)
-
+#
     for lines in alldata:
         netCOH.append(int(lines[1]))
         allCOH.append({lines[0]: int(lines[1])})
         
     deficit = []
-
+#
     for index, value in enumerate(netCOH):
         if value > netCOH[0]:
             diff = netCOH[index] - netCOH[index-1]
             if diff < 0 :
                 deficit.append([value, diff])
-            
+#        
     list =[]
-
+#
     for items in allCOH:
         for days, number in items.items():
             for one, two in deficit:
